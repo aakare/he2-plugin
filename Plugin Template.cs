86966d6bba4b2fc2730bb1c59664bc6840d24458
aakare <45142477+aakare@users.noreply.github.com>
2019-04-28 20:41:02 +0200

    Add files via upload

---
+++ Plugin Template.cs
@@ -0,0 +1,28 @@
+using System;
+using HePlug; // plugin library
+
+namespace He2Plugin
+{
+    public class (classname) : Plugin
+    {
+        public void Code(string[] args)
+        {
+            (code)
+        }
+        //description
+        public string Description()
+        {
+            return (description);
+        }
+        //command
+        public string Name()
+        {
+            return (name);
+        }
+        //help
+        public string Help()
+        {
+            return (help);
+        }
+    }
+}
